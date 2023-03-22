b2b9f3a8b2109dee4768ff707872ffdb8c983bda
Sundus <sundusaden105@gmail.com>
2023-03-22 06:17:27 +0300

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+def index
+    items = Item.order(created_at: :desc).includes(:user)
+    render json: items, include: :user
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        usr = User.find(params[:id])
+        render json: usr, include: :items
+      end
 end
